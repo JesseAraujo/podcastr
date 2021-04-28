05564d0e08c4b3240c4a2c54af26c8725927be66
JesseAraujo <jesse.araujo4@fatec.sp.gov.br>
2021-04-28 07:18:38 -0300

    finalizando "ultimos lançamentos"

---
+++ src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,13 @@
+export function convertDurationToTimeString(duration: number) {
+    const hours = Math.floor(duration / 3600) //arredonda para baixo
+    const minutes = Math.floor((duration % 3600) / 60)
+    const seconds = duration % 60
+
+    const timeString = [hours, minutes, seconds]
+        .map(unit => String(unit).padStart(2, '0')) //acrescentará 0 na frente, ex: 1 ficará 01
+        .join(':')
+
+
+    return timeString
+
+}
